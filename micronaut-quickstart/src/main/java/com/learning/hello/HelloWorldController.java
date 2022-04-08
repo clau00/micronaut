1b94756482e1e39cfef9a4df8ad96a9c7707052d
Claudiu Balint <cbalint@deloittece.com>
2022-04-08 11:23:47 +0300

    CB: micronaut: configuration injection + translation

--- micronaut-quickstart/src/main/java/com/learning/hello/HelloWorldController.java
+++ micronaut-quickstart/src/main/java/com/learning/hello/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.learning.hello;
 
+import io.micronaut.context.annotation.Property;
 import io.micronaut.http.MediaType;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
@@ -12,14 +13,30 @@ public class HelloWorldController {
     private static final Logger LOG = LoggerFactory.getLogger(HelloWorldController.class);
 
     private final MyService service;
+    private final String helloFromConfig;
+    private final HelloWorldTranslationConfig translationConfig;
 
-    public HelloWorldController(MyService service) {
+    public HelloWorldController(MyService service, @Property(name = "hello.world.message") String helloFromConfig,
+                                HelloWorldTranslationConfig translationConfig) {
         this.service = service;
+        this.helloFromConfig = helloFromConfig;
+        this.translationConfig = translationConfig;
     }
 
     @Get(produces = MediaType.TEXT_PLAIN)
     public String helloWorld() {
         LOG.debug("Called the hello world API");
         return service.helloWorldService();
+    }
+
+    @Get(value = "/config", produces = MediaType.TEXT_PLAIN)
+    public String helloFromConfig() {
+        LOG.debug("Return Hello From Config Message: {}", helloFromConfig);
+        return helloFromConfig;
+    }
+
+    @Get(value = "/translation", produces = MediaType.APPLICATION_JSON)
+    public HelloWorldTranslationConfig helloTranslation() {
+        return translationConfig;
     }
 }
--- micronaut-quickstart/src/test/java/com/learning/HelloWorldControllerTest.java
+++ micronaut-quickstart/src/test/java/com/learning/HelloWorldControllerTest.java
@@ -1,5 +1,6 @@
 package com.learning;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import io.micronaut.http.HttpStatus;
 import io.micronaut.http.client.HttpClient;
 import io.micronaut.http.client.annotation.Client;
@@ -29,5 +30,19 @@ public class HelloWorldControllerTest {
         var response = client.toBlocking().exchange("/hello", String.class);
         assertEquals("Hello World from Service!", response.getBody().get());
         assertEquals(HttpStatus.OK, response.getStatus());
+    }
+
+    @Test
+    void helloWorldEndpointReturnsMessageFromConfigFile() {
+        var response = client.toBlocking().exchange("/hello/config", String.class);
+        assertEquals("Hello from application.yml", response.getBody().get());
+        assertEquals(HttpStatus.OK, response.getStatus());
+    }
+
+    @Test
+    void helloFromTranslationEndpointReturnsMessageFromConfigFile() {
+        var response = client.toBlocking().exchange("/hello/translation", JsonNode.class);
+        assertEquals(HttpStatus.OK, response.getStatus());
+        assertEquals("{\"de\":\"Hallo Welt\",\"en\":\"Hello World\"}", response.getBody().get().toString());
     }
 }
